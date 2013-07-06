06d328f6b99c827f305893043fc909a0ae416f88
Chris Ruppel <chris.ruppel@gmail.com>
2013-07-06 01:52:46 -0500

    Creating Sass assets and converting current CSS

---
+++ static/config.rb
@@ -0,0 +1,18 @@
+require 'compass'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# To disable debugging comments that display the original location of your
+# selectors. Uncomment:
+line_comments = false
+
+# Cache buster shows up as a random querystring at the end of asset filenames.
+asset_cache_buster :none
+
+# Output style controls whitespace in compiled CSS
+output_style = :compressed # :expanded or :nested or :compact or :compressed
